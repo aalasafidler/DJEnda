32964a05e49969726a69ee46a57c2ca14f57cd57
Aalasa Fidler <aalasa.fidler@ucdconnect.ie>
2019-04-27 14:49:04 +0100

    Split website up into pages

--- DJEnda/urls.py
+++ DJEnda/urls.py
@@ -21,6 +21,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('about', views.about),
+        path('gallery', views.gallery),
+                path('contact', views.contact),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- DJEnda/views.py
+++ DJEnda/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'index.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def gallery(request):
+	return render(request, 'gallery.html')
+
+def contact(request):
+	return render(request, 'contact.html')
